d634d3682687d670328eba7efb4ace8726cee597
Ian <isibley@purdue.edu>
2016-09-25 20:01:32 +0100

    Second Draft (working)

--- move.js
+++ move.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
-	var gp = navigator.getGamepads();
-	var div = $("#moving");
-	var mes = $("#messages");
-	mes.text(Object.getOwnPropertyNames(gp));
+  setInterval(function() {
+    var gp = navigator.getGamepads();
+    var div = $("#moving");
+    var mes = $("#messages");
 
-	//while(1) {
-		gp = navigator.getGamepads();
-		if(gp != null && gp != undefined) {
-			div.text("good");
-			gp.a = gp[0].buttons[0].pressed;
-			gp.b = gp[0].buttons[1].pressed;
-			gp.x_ax = gp[0].axes[0];
-			gp.y_ax = gp[0].axes[1];
+    gp = navigator.getGamepads();
+    if(gp != null && gp != undefined) {
+      gp.a = gp[0].buttons[0].pressed;
+      gp.b = gp[0].buttons[1].pressed;
+      gp.x_ax = gp[0].axes[0];
+      gp.y_ax = gp[0].axes[1];
+      mes.text("A: "+gp.a+"    B: "+gp.b+"    X-axis: "+parseInt(gp.x_ax*10)+"    Y-axix: "+parseInt(gp.y_ax*10));
 
-			if(gp.a && !gp.b) {
-				div.css("background-color", "cyan");
-			} else if(gp.b && !gp.a) {
-				div.css("background-color", "red");
-			}
-			
-			
-			div.css({top: "+="+(10*gp.y_ax).toFixed(0)+"px"});
-			div.css({left: "+="+(10*gp.x_ax).toFixed(0)+"px"});
-		} else {
-			div.text("bad");
-			mes.append("\n"+(gp == null)+"\n"+(gp == undefined));
-		}
-	//}
+      if(gp.a && !gp.b) {
+        div.css("background-color", "cyan");
+      } else if(gp.b && !gp.a) {
+        div.css("background-color", "red");
+      }
+
+      
+
+      div.css({top: "+="+parseInt(10*gp.y_ax)+"px"});
+      div.css({left: "+="+parseInt(10*gp.x_ax)+"px"});
+    } else {
+      mes.text("No Gamepads Found");
+    }
+  }, 1);
 });
