b0238b1f4a72c4f717a7d2d1d01180affe804f6a
AnotherOctopus <li1735@purdue.edu>
2016-10-09 22:19:39 -0400

    added the uptimeModule, reoginized

--- control/timeS.py
+++ control/timeS.py
@@ -1,7 +1,6 @@
 ll__ = ["monotonic_time"]
 
 import ctypes, os
-import RPi.GPIO as GPIO
 
 CLOCK_MONOTONIC_RAW = 4 # see <linux/time.h>
 
@@ -15,23 +14,12 @@ librt = ctypes.CDLL('librt.so.1', use_errno=True)
 clock_gettime = librt.clock_gettime
 clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(timespec)]
 
-def monotonic_time():
+def absTime():
     t = timespec()
     if clock_gettime(CLOCK_MONOTONIC_RAW , ctypes.pointer(t)) != 0:
         errno_ = ctypes.get_errno()
         raise OSError(errno_, os.strerror(errno_))
     return t.tv_sec + t.tv_nsec * 1e-9
 
-message= "(HEY GO, YOU GOT TO CHANGE THIS CODE SO ALL IT DOES IS MAKE IT SO YOU CAN EASILY ACCES THE TIME. WE DONT NEED ALL OF THIS INFINATE LOOOP JUNK. OR THE TURNING ON OF THE GPIO, JUST MAKE IT SO WE CAN RUN A QUICK THING) ALSO DELETE THIS, I CANT COMMENT WITH THIS KEYBOARD"
-if __name__ == "__main__":
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(14,GPIO.OUT)
-	t=0
-	while (1==1):
-    		t = monotonic_time()
-		
-		if(int(t*1000)%2==1):
-			GPIO.output(14,GPIO.HIGH)
-		else:	
-			GPIO.output(14,GPIO.LOW)
+def TimeStep(prevTime, newTime):
+    return newTime-prevTime
