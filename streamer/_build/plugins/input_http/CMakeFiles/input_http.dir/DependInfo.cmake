463a30ab6103ed4eab8c911e145b360d60fda7cf
Ajay1997 <ajayGkumar97@gmail.com>
2016-10-15 16:42:26 -0400

    Added streamer code

---
+++ streamer/_build/plugins/input_http/CMakeFiles/input_http.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ajay/mjpg-streamer/mjpg-streamer-experimental/plugins/input_http/input_http.c" "/home/ajay/mjpg-streamer/mjpg-streamer-experimental/_build/plugins/input_http/CMakeFiles/input_http.dir/input_http.c.o"
+  "/home/ajay/mjpg-streamer/mjpg-streamer-experimental/plugins/input_http/misc.c" "/home/ajay/mjpg-streamer/mjpg-streamer-experimental/_build/plugins/input_http/CMakeFiles/input_http.dir/misc.c.o"
+  "/home/ajay/mjpg-streamer/mjpg-streamer-experimental/plugins/input_http/mjpg-proxy.c" "/home/ajay/mjpg-streamer/mjpg-streamer-experimental/_build/plugins/input_http/CMakeFiles/input_http.dir/mjpg-proxy.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
