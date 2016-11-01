dd9d27e9f8a5036a49a394e30296c26ee5f8dc01
AnotherOctopus <li1735@purdue.edu>
2016-11-01 11:36:45 -0400

    added test bench for Kfilter

--- control/stateTogether/kalmanTest.py
+++ control/stateTogether/kalmanTest.py
@@ -1,7 +1,33 @@
 from kalmanFilter import kalman
+import matplotlib.pyplot as plt
+from time import sleep
 craft = kalman(10,10,10,10,10,10,10,10,10,10,10,10)
 print craft.xVar
 print craft.stateX
-craft.update(1,1,1,1,1,1)
+t = 0
+dT =0.01
+xPos = []
+xPos_act = 1
+craft.stateX[0]=1
+
+xPos_act_plot =[]
+vel_act = 0
+while (t < 200):
+    xPos_act_plot.append(xPos_act)
+    if t < 100:
+        craft.update(1,1,1,1,1,1)
+        vel_act = vel_act+1*dT
+    else:
+        craft.update(-1,-1,-1,-1,-1,-1)
+        vel_act = vel_act-1*dT
+    xPos_act = xPos_act+vel_act*dT
+    t = t + 1;
+    xPos.append(craft.stateX[0])
+    sleep(dT)
+
 print craft.xVar
-print craft.stateX
+print craft.stateX[0]
+plt.plot(xPos)
+plt.plot(xPos_act_plot)
+plt.show()
+
