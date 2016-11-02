604969e456bbc05f5edcb70774fe5969fcff8fe5
samjenks <jenkin49@purdue.edu>
2016-11-02 11:09:10 -0400

    Update SensorClasses.py
    added self and a return because i'm dumb

--- sensors/SensorClass/SensorClasses.py
+++ sensors/SensorClass/SensorClasses.py
@@ -74,56 +74,56 @@ class Pressure(object):
         self.bus = smbus.SMBus(1)
         
     def get_pressure(self):
-        bus.write_byte(0x76, 0x1E)
+        self.bus.write_byte(0x76, 0x1E)
         time.sleep(0.5)
 
         # Read 12 bytes of calibration data
         # Read pressure sensitivity
-        data = bus.read_i2c_block_data(0x76, 0xA2, 2)
+        data = self.bus.read_i2c_block_data(0x76, 0xA2, 2)
         C1 = data[0] * 256 + data[1]
 
         # Read pressure offset
-        data = bus.read_i2c_block_data(0x76, 0xA4, 2)
+        data = self.bus.read_i2c_block_data(0x76, 0xA4, 2)
         C2 = data[0] * 256 + data[1]
 
         # Read temperature coefficient of pressure sensitivity
-        data = bus.read_i2c_block_data(0x76, 0xA6, 2)
+        data = self.bus.read_i2c_block_data(0x76, 0xA6, 2)
         C3 = data[0] * 256 + data[1]
 
         # Read temperature coefficient of pressure offset
-        data = bus.read_i2c_block_data(0x76, 0xA8, 2)
+        data = self.bus.read_i2c_block_data(0x76, 0xA8, 2)
         C4 = data[0] * 256 + data[1]
 
         # Read reference temperature
-        data = bus.read_i2c_block_data(0x76, 0xAA, 2)
+        data = self.bus.read_i2c_block_data(0x76, 0xAA, 2)
         C5 = data[0] * 256 + data[1]
 
         # Read temperature coefficient of the temperature
-        data = bus.read_i2c_block_data(0x76, 0xAC, 2)
+        data = self.bus.read_i2c_block_data(0x76, 0xAC, 2)
         C6 = data[0] * 256 + data[1]
 
         # MS5837_30BA01 address, 0x76(118)
         #		0x40(64)	Pressure conversion(OSR = 256) command
-        bus.write_byte(0x76, 0x40)
+        self.bus.write_byte(0x76, 0x40)
 
         time.sleep(0.5)
 
         # Read digital pressure value
         # Read data back from 0x00(0), 3 bytes
         # D1 MSB2, D1 MSB1, D1 LSB
-        value = bus.read_i2c_block_data(0x76, 0x00, 3)
+        value = self.bus.read_i2c_block_data(0x76, 0x00, 3)
         D1 = value[0] * 65536 + value[1] * 256 + value[2]
 
         # MS5837_30BA01 address, 0x76(118)
         #		0x50(64)	Temperature conversion(OSR = 256) command
-        bus.write_byte(0x76, 0x50)
+        self.bus.write_byte(0x76, 0x50)
 
         time.sleep(0.5)
 
         # Read digital temperature value
         # Read data back from 0x00(0), 3 bytes
         # D2 MSB2, D2 MSB1, D2 LSB
-        value = bus.read_i2c_block_data(0x76, 0x00, 3)
+        value = self.bus.read_i2c_block_data(0x76, 0x00, 3)
         D2 = value[0] * 65536 + value[1] * 256 + value[2]
 
         dT = D2 - C5 * 256
@@ -152,6 +152,8 @@ class Pressure(object):
         pressure = ((((D1 * SENS2) / 2097152) - OFF2) / 8192) / 10.0
         cTemp = TEMP / 100.0
         fTemp = cTemp * 1.8 + 32
+        
+        return [pressure, cTemp, fTemp]
 
     
     
