043ea9de7d0bc37bc67056d48b36e73adcd298e5
GoGirish <girishg@purdue.edu>
2016-11-12 16:00:33 -0500

    Update CameraServoWrapper.py
    Added basic function to set raw angle. Will start writing actual servo class first

--- control/CameraServoWrapper.py
+++ control/CameraServoWrapper.py
@@ -3,4 +3,19 @@ import sys
 import time
 import smbus
 
-class CameraServoWrapper.py
+#ASSUME that you are using a Servo Class created by Power and Movement that sets the servo to a raw angle.
+#ASSUME that servo has a function called setAngle which sets the servo angle appropriately.
+
+class CameraServoWrapper(object):
+  def __init__(self):
+    self.servo = ##ADD THE SERVO here
+    self.servo.begin()
+    #The angle is the raw angle from servo.
+    self.servo.angle = 0;
+    #The reference angle is the angle we SET zero
+    self.servo.referenceAngleZero = 0;
+    
+  def setRawAngle(self, angle):
+    #sets the raw angle of the servo appropriately
+    self.servo.setAnlgle(angle)
+    self.Servo.angle = angle
