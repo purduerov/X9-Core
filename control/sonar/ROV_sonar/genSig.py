3914990ce7bc0f5a34dd3b5e104ea5c5fe50821d
anotheroctopus <li1735@purdue.du>
2016-11-23 01:40:44 -0500

    added faster analog reads

--- control/sonar/ROV_sonar/genSig.py
+++ control/sonar/ROV_sonar/genSig.py
@@ -1,12 +1,21 @@
+import math
 arrayLength = 1000
+max = 255
+numPulse = 5
+freq = 8000
+
 def genArray(length):
 	x = 0;
-	toPrint = str(x)
 
+	toPrint = str(math.floor(math.sin(x*2*math.pi/100)*max/2)+max/2)
 	while (x<(length-1)):
 		x = x + 1
+		if(x < 500):
+			sample = math.floor(math.sin(x*2*math.pi/100)*max/2)+max/2
+		else:
+			sample = max/2
 		toPrint = toPrint + ','
-		toPrint = toPrint + str(x)
+		toPrint = toPrint + str(sample)
 	toPrint = toPrint + '};'
 	return toPrint
 
@@ -16,7 +25,7 @@ with open('ROV_sonar.ino','r') as file:
 	data = file.readlines()
 
 array = array + genArray(arrayLength) + '\n'
-data[26] = array
+data[25] = array
 print array
 with open('ROV_sonar.ino','w') as file:
 	file.writelines(data)
