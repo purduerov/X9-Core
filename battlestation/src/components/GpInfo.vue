d1e49e615d5e4310ea8deb2e41bd0b9a26af2b1c
Ian <isibley@purdue.edu>
2016-12-07 01:01:40 +0000

    GPInfo.vue 'working' while not working

--- battlestation/src/components/GpInfo.vue
+++ battlestation/src/components/GpInfo.vue
@@ -3,18 +3,20 @@
     <p>{{ gphead }}</p>
     <ul id="btn-status">
       <li v-for="item in buttons">
-        {{ buttons.name+": "+buttons.val }}
+        {{ item.name+": "+item.val }}
       </li>
     </ul>
+  <!-- 
     <ul id="axes-status">
       <li v-for="item in axes">
-        <p>{{ axes.name+":" }}</p>
-        <p class="gp_ax_sub">{{ axes.left.x }}</p>
-        <p class="gp_ax_sub">{{ axes.left.y }}</p>
-        <p class="gp_ax_sub">{{ axes.left.r }}</p>
-        <p class="gp_ax_sub">{{ axes.left.theta }}</p>
+        <p>{{ item.name+":" }}</p>
+        <p class="gp_ax_sub">{{ item.stats.x }}</p>
+        <p class="gp_ax_sub">{{ item.stats.y }}</p>
+        <p class="gp_ax_sub">{{ item.stats.r }}</p>
+        <p class="gp_ax_sub">{{ item.stats.theta }}</p>
       </li>
     </ul>
+    -->
   </div>
 </template>
 
@@ -25,24 +27,24 @@
     props: ['gphead'],
     data: function() {
       return {
-        buttons: [],
-        axes: []
+        buttons: [{name: "a", val: 0}, {name: "b", val: 3}]//,
+        //axes: []
         }
       },
     methods: {
       update_gp: function() {
           var that = this;
-          console.log(this.buttons);
-          console.log(this.axes);
+          //console.log(this.buttons);
+          //console.log(this.axes);
           Object.keys(gp.buttons).forEach(function(key_b, i) {
             that.buttons[i] = { name: key_b, val: gp.buttons[key_b].val };
-          });
+          });/*
           Object.keys(gp.axes).forEach(function(key_a, i) { //updates the axes 
             that.axes[i] = {name: key_a, stats: {} };
             Object.keys(gp.axes[key_a]).forEach(function(key_s, j) {
               that.axes[i].stats[key_s] = gp.axes[key_a][key_s];
             });
-          });
+          });*/
       }
     },
     mounted: function() {
