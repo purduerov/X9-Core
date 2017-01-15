f99777c0e7858288a178b3f13881f2eb5b8b1926
syigrem <syigrem@purdue.edu>
2017-01-15 16:54:21 -0500

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,7 +1,6 @@
 import subprocess
 import os
 
-
 class Camera:
 
     # In order to run mjpg-streamer through Python, make sure
@@ -11,7 +10,7 @@ class Camera:
     # files not being able to be found. Resolution must also be
     # specified in "integerxinteger" and not by name.
 
-    # default layout for camera, allows only one camera to function at any point in time unless changed in changeSet method
+    # default layout for camera
     def __init__(self, resolution='1024x768', framerate=30, device='/dev/video0', port='8080', brightness=16, contrast=32):
 
         self.switch = None
@@ -24,26 +23,13 @@ class Camera:
         self.inp = 'input_uvc.so -r ' + self.r + ' -f ' + str(self.f) + ' -d ' + self.d + ' -br ' + str(self.b) + ' -co ' + str(self.c)
         self.out = 'output_http.so -w /usr/local/www -p ' + str(self.p)
 
-    # allows the resolution, framerate, device and port to be changed, also the only
-    # way to have multiple cameras working at once resolution is put in the format of
-    # a string: "VGA" - 640x480, "PAL" - 768x576, "SVGA" - 800x600, "WXGA" - 1280x720
-    # framerate shouldn't be changed: keep at 30, allows for a good image while
-    # reserving valuable processing power for other devices device is formatted as a
-    # string: /dev/videoNUM where NUM is the number for the order in which camera is
-    # plugged in, starting at 0 port is the web port where you want to output image
-    # to: change as needed
-    def changeSet(self, resolution, framerate, device, port):
-        self.r = resolution
-        self.f = framerate
-        self.d = device
-        self.p = port
-        self.inp = 'input_uvc.so -r ' + self.r + ' -f ' + str(self.f) + ' -d ' + self.d + ' -br ' + str(self.b) + ' -co ' + str(self.c)
-        self.out = 'output_http.so -w /usr/local/www -p ' + str(self.p)
+   
 
-    # resolution is in string format: "VGA" - 640x480, "PAL" - 768x576, "SVGA" - 800x600, "WXGA" - 1280x720,
-    # framerate shouldn't be changed: keep at 24 unless necessary change, allows for a good image while reserving processing power,
-    # device is in string format: /dev/videoNUM where NUM is the number for the order in which camera is plugged in,
-    # port is the web port where you want to output image to: change as needed
+    # framerate shouldn't be changed: keep at 30, allows for a good image while
+    # reserving valuable processing power for other devices. Device is formatted as a
+    # string: /dev/videoNUM where NUM is the number for the order in which camera is
+    # plugged in, starting at 0. Port is the web port where you want to output image
+    # to: change as needed
 
     # open video feed for an instance of Camera
     def on(self):
