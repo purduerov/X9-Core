3a2b247d8c2d12d9fd6e7ea760d6be9c45c27885
samjenks <jenkin49@purdue.edu>
2017-01-21 14:55:29 -0800

    Update Accel_to_Pos.py
    functionality edits

--- control/PID/Accel_to_Pos.py
+++ control/PID/Accel_to_Pos.py
@@ -1,12 +1,11 @@
 import time
 
-
-class AccelConversion(object):
-
-        def __init__(self):
-            self.time_start = time.time()
+class Accel_to_Pos(object):
 
         def integration(self, accel):
             delta_t = time.time() - self.time_start
             pos = accel * (delta_t ** 2) # assumes the ROV has 0 velocity at the time of the switch to autopilot
             return pos
+
+        def start_over(self):
+            self.time_start = time.time()
