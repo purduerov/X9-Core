f96d1626fd3dd99266838ed4f9b578dbc73de878
Ajay1997 <ajayGkumar97@gmail.com>
2017-01-23 20:11:06 -0500

    serial to i2c coomunication for arduino like board

---
+++ packet_data.c/packet_data.c.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+char incomingByte;  // incoming data
+int  LED = 13;      // LED pin
+char n;
+char c = 0;
+int* packetdata[8] = {0};
+ 
+void setup() {
+  Serial.begin(9600); // initialization
+  Wire.begin();   
+   while (!Serial.available()){
+    incomingByte = Serial.read();
+    if(incomingByte == '5'){
+        c = 0;
+        Serial.println("Changed");
+       }
+  }
+}
+
+void sendi2c(int* packetdata) {
+   // transmit to device #9
+  for(int i = 0;i<10;i++){
+    Wire.beginTransmission(9);
+    Wire.write(packetdata[i]);    
+    Wire.endTransmission();
+    delay(10);
+  }
+      // stop transmitting  
+ 
+}
+ 
+void loop() {  
+  Serial.flush();
+  while(c== 0){
+    if (Serial.available() > 0) {  // if the data came
+    incomingByte = Serial.read(); // read byte   
+    }
+  }
+}
