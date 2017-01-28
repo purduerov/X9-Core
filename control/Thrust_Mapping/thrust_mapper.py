c5ffe80bb868e455d949a0eb663f5dfc673f0c27
AndrewB <bostockdrew97@gmail.com>
2017-01-28 10:50:54 -0500

    updated to use the new matrix generator

--- control/Thrust_Mapping/thrust_mapper.py
+++ control/Thrust_Mapping/thrust_mapper.py
@@ -1,6 +1,6 @@
 import numpy as np
-import mutatormatrix 
-
+# import mutatormatrix 
+import thrustMapperMatrix as mm
 
 class ThrustMapper(object):
     """Class to be used to generate the desired thrust map based on the current and desired position"""
@@ -20,11 +20,15 @@ class ThrustMapper(object):
     """
     def __init__(self):
         self.thrustMap = np.array([0, 0, 0, 0, 0, 0])   # list for values that need to be output to the thrusters
-        self.maker = mutatormatrix.MutatorMatrix(3, -1.5, 0, 3, 1, 0,
-                                             -2, -1.5, 0, -2, 1, 0,
-                                             1.5, -1, 1, 1.5, 0.5, 1,
-                                             -1, -1, 1, -1, 0.5, 1)
-        self.mutationMatrix = self.maker.generate_matrix()
+        self.maker = mm.MutatorMatrix()
+        """self.maker = mutatormatrix.MutatorMatrix(-4.7524,0,11.2148,4.7524,0,11.2148,
+                                             -4.7524,0,-7.631,4.7524,0,-7.631,
+                                             -5.5709,5.6384,6.0419,
+                                             5.5709,5.6384,6.0419,
+                                             -5.5709,5.6384,-2.485,
+                                             5.5709,5.6384,-2.485)"""
+        # self.mutationMatrix = self.maker.generate_matrix()
+        self.mutationMatrix = self.maker.generateMatrix()
 
     def generate_thrust_map(self, desired):
         """generate the thrustMap to move to the desired vector
@@ -34,4 +38,8 @@ class ThrustMapper(object):
         # some math to determine the thrustMap
         self.thrustMap = self.mutationMatrix.dot(desired)
         return self.thrustMap
+
+
+if __name__ == "__main__":
+    print ThrustMapper().mutationMatrix
 
