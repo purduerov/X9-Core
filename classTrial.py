2e0cc1dc6c5026068c4310bb38334a7b94c4f5b0
Dominic Miller <mill2012@purdue.edu>
2017-02-15 16:45:40 +0000

    finished with the stats libraries and tested them in the main

--- classTrial.py
+++ classTrial.py
@@ -1,64 +1,70 @@
 import subprocess
 import time
 
+#class RasPiStats():
+
+x = None
+y = None
+z = None
+avg = None
+u = None
+
 class RasPiStats():
+        
+        def __init__(self):
+            pass
 
-        x = None
-        y = None
-        z = None
-        avg = None
-        u = None
+        def startTemp(self):#need to parse to get only the number(outputs temp = x)
+            x =(subprocess.Popen(['vcgencmd','measure_temp']))
+		#returM-}                     Indent the current linen x.split('=')[1]#-----problem here, triple '
 
-	def startTemp():#need to parse to get only the number(outputs temp = x)
-		x =(subprocess.Popen(['vcgencmd','measure_temp'], PIPE))
-		#return x.split('=')[1]#-----problem here, triple '
-
-	def startClockRate():
-		y =(subprocess.Popen(['cat','/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq']))
+	def startClockRate(self) :
+            y =(subprocess.Popen(['cat','/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq']))
 		#return y
 
-	def startVolt():
-		z =(subprocess.Popen(['vcgencmd','measure_volts']))
+        def startVolt(self):
+            z =(subprocess.Popen(['vcgencmd','measure_volts']))
 		#return z.split('=')[1]
 
-	def startLoadAvg():
-		avg = (subprocess.Popen(['cat', '/proc/loadavg']))
+        def startLoadAvg(self):
+            avg = (subprocess.Popen(['cat', '/proc/loadavg']))
 		#return avg
 
-	def startMemUsed():
-		u = (subprocess.Popen(['du', '-hs']))#, '/dev/root']))
+        def startMemUsed(self):
+	    u = (subprocess.Popen(['du', '-hs']))#, '/dev/root']))
+            u = u.split(' ')[1]
 
-        def getTemp():
+
+        def getTemp(self):
             if x != None and x.poll()==True:
                 # TODO: if stdout is not reporting output, change popen constructor argument stdout=PIPE //to do this put it outside array brackets but within parenthesis([~~~~], PIPE) 
                 for line in x.stdout:
                     print(line)
-                x = None
-                return True
-            return False
+               
+               #return True
+            #return False
 
-
-        def getClockRate():
+        def getClockRate(self):
             if y != None and y.poll()==True:
                 for line in y.stdout:
                     print(line) #save text somewhere else
 
-        def getVolt():
+        def getVolt(self):
             if z != None and z.poll()==True:
                 for line in z.stdout:
                     print(line)
         
-        def getLoadAvg():
+        def getLoadAvg(self):
             if avg != None and avg.poll()==True:
                 for line in avg.stdout:
                     print(line)
 
-        def getMemUsed():
+        def getMemUsed(self):
             if u != None and u.poll()==True:
                 for line in u.stdout:
                     print(line)
 
-	def endFunctions(id=0):
+        def endFunctions(id=0):
             if id==0 and (x != None and y != None and z != None and avg != None and u != None):
                 # TODO: change terminate to kill
                 x.terminate()
@@ -77,18 +83,34 @@ class RasPiStats():
             if id==5:
                 u.terminate()
 
-#print getTemp()
-#print getVolt()
-#print getClockRate()
-#print getLoadAvg()
-#print getMemUsed()
-#getMemUsed()
-
-if __name__ == '__main__': #if the name of the class is the same as the one being called, run this code
+def main():
     test = RasPiStats()
 
     test.startTemp()
+    test.getTemp()
 
-    while test.getTemp() != False:
-        pass
-    #test.endFunctions(0)
+    test.startMemUsed()
+    test.getMemUsed()
+
+    test.startTemp()
+    test.getTemp()
+
+    test.startVolt()
+    test.getVolt()
+
+    test.startClockRate()
+    test.getClockRate()
+    
+
+if __name__ == '__main__':main() #if the name of the class is the same as the one being called, run this code
+    
+   #def main():
+
+        #test = RasPiStats()
+
+        #test.startTemp()
+	
+	#print 
+        #while test.getTemp() != False:
+            #pass
+            #test.endFunctions(0)
