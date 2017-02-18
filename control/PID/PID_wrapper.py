c0e992a6bad04828fd0d8d4ea2f58bf6e32fba34
Andrew Bostock <Andrew Bostock>
2017-02-18 11:44:43 -0500

    New PID wrapper for locking inputs

--- control/PID/PID_wrapper.py
+++ control/PID/PID_wrapper.py
@@ -14,24 +14,78 @@ class PID(object):
         self.phi = PID_controller.PID_Controller()
         self.mu = PID_controller.PID_Controller()
         self.theta = PID_controller.PID_Controller()
-        self.prevOut = np.array([[0], [0], [0], [0], [0], [0]])
+        self.locked_x = 0
+        self.locked_y = 0
 
     def getDesired(self, direction, locked):
         desired = np.array([[0], [0], [0], [0], [0], [0]])
-        desired[0, 0] = self.evalStatisLock(direction[0, 0], locked[0], self.locked[0],
-                                            self.prevOut[0, 0])
-        desired[0, 1] = self.evalStatisLock(direction[0, 1], locked[1], self.locked[1],
-                                            self.prevOut[0, 1])
+        desired[0, 0] = self.evalX(direction[0, 0], locked[0])
+        desired[0, 1] = self.evalY(direction[0, 1], locked[1])
+        desired[0, 2] = self.evalZ(direction[0, 2], locked[2])
+        desired[0, 3] = self.evalPhi(direction[0, 3], locked[3])
+        desired[0, 4] = self.evalMu(direction[0, 4], locked[4])
+        desired[0, 5] = self.evalTheta(direction[0, 5], locked[5])
 
         self.locked = locked
-        self.prevOut = desired
+        return desired
 
-    def evalStatisLock(self, desired, locked, prevLockStatus, prevOut):
+    def evalX(self, desired, locked):
         if locked == 0:
             return desired
-        elif prevLockStatus == 1:
-            return prevOut
+        elif self.locked[0] == 1:
+            return self.locked_x
         else:
-            return desired
+            self.locked_x = desired
+            return self.locked_x
 
-    def evalAbsoluteLock(self):
+    def evalY(self, desired, locked):
+        if locked == 0:
+            return desired
+        elif self.locked[1] == 1:
+            return self.locked_y
+        else:
+            self.locked_y = desired
+            return self.locked_y
+
+    def evalZ(self, desired, locked):
+        if locked == 0:
+            return desired
+        elif self.locked[2] == 1:
+            self.z.update(self.data.get_state())
+            return self.z.getOutput()
+        else:
+            self.z.set_setpoint(self.data.get_state())
+            return self.z.update(self.data.get_state())
+
+    def evalPhi(self, desired, locked):
+        if locked == 0:
+            return desired
+        elif self.locked[3] == 1:
+            self.phi.update(self.data.get_state('Roll'))
+            return self.phi.getOutput()
+        else:
+            self.phi.set_setpoint(self.data.get_state('Roll'))
+            self.phi.update(self.data.get_state('Roll'))
+            return self.phi.getOutput()
+
+    def evalMu(self, desired, locked):
+        if locked == 0:
+            return desired
+        elif self.locked[3] == 1:
+            self.mu.update(self.data.get_state('Pitch'))
+            return self.mu.getOutput()
+        else:
+            self.mu.set_setpoint(self.data.get_state('Pitch'))
+            self.mu.update(self.data.get_state('Pitch'))
+            return self.mu.getOutput()
+
+    def evalTheta(self, desired, locked):
+        if locked == 0:
+            return desired
+        elif self.locked[3] == 1:
+            self.theta.update(self.data.get_state('Heading'))
+            return self.theta.getOutput()
+        else:
+            self.theta.set_setpoint(self.data.get_state('Heading'))
+            self.theta.update(self.data.get_state('Heading'))
+            return self.theta.getOutput()
