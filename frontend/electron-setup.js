494a207217c1b7dce2356b6b911aef462546677c
Kyle <krakos@purdue.edu>
2017-04-30 15:27:02 -0400

    Commented out some unworking start lines

--- frontend/electron-setup.js
+++ frontend/electron-setup.js
@@ -7,8 +7,8 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
-const fs = require('fs')
-const ipc = require('ipc')
+//const fs = require('fs')
+//const ipc = require('ipc')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
