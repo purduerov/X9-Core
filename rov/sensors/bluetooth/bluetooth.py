e24e06cc73ff69522f8316d1c7d01f0dc383c977
Andrew <Bostockdrew97@gmail.com>
2017-06-22 22:10:17 -0400

    bluetooth?

---
+++ rov/sensors/bluetooth/bluetooth.py
@@ -0,0 +1,17 @@
+import serial
+
+
+class Bluetooth(object):
+
+    def __init__(self):
+        self.bluetooth_serial = serial.Serial()
+        self.bluetooth_serial.open()
+
+    def read(self):
+        res = ''
+        i = 7
+        while self.bluetooth_serial.in_waiting and i != 0:
+            res = res + self.bluetooth_serial.read()
+            i += 1
+        res += "\n"
+        return res
